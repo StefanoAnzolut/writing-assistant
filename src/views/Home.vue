1f5900a4a5f2bfc888072f5ac68331ddc3453abf
Stefano Anzolut <stefano@skribble.com>
2023-10-20 17:13:32 +0200

    Initialize repo with vuetify-vue template

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <ChatInterface />
+  <TextEditor />
+</template>
+
+<script setup>
+  import ChatInterface from '../components/ChatInterface.vue';
+  import TextEditor from "../components/TextEditor.vue";
+</script>
